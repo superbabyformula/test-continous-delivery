aca213cac3264041504d79a0f328bf6cd2003834
superbabyformula <andrew.j.worzella@gmail.com>
2018-10-12 03:32:46 +0200

    Added jacoco code coverage report and check

--- calculator/src/main/java/com/ajw/calculator/calculator/CalculatorApplication.java
+++ calculator/src/main/java/com/ajw/calculator/calculator/CalculatorApplication.java
@@ -3,10 +3,22 @@ package com.ajw.calculator.calculator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * A simple calculator application used solely for the purpose of setting up a 
+ * continuous delivery pipeline.
+ * <p>
+ * The application is a Spring Boot Application.
+ * </p>
+ * @author andy
+ */
 @SpringBootApplication
 public class CalculatorApplication {
 
-	public static void main(String[] args) {
+    /**
+     *
+     * @param args Runtime arguments
+     */
+    public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
 	}
 }
