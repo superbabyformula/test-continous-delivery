1512b5ab3b316721ad8c88a321a40c18203697cb
superbabyformula <andrew.j.worzella@gmail.com>
2018-10-12 05:25:23 +0200

    Added unit tests

---
+++ calculator/src/main/java/com/ajw/calculator/calculator/Calculator.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ajw.calculator.calculator;
+
+/**
+ *
+ * @author andy
+ */
+public class Calculator {
+    public int sum(int x, int y) {
+        return x+y;
+    }
+}
---
+++ calculator/src/test/java/com/ajw/calculator/calculator/CalculatorApplicationTest.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ajw.calculator.calculator;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author andy
+ */
+public class CalculatorApplicationTest {
+    
+    @Test
+    public void testCalculatorApplication() {
+        CalculatorApplication app = new CalculatorApplication();
+        Assert.assertEquals("", "");
+    }
+    
+    @Test
+    public void testSum() {
+        Calculator calculator = new Calculator();
+        Assert.assertEquals(calculator.sum(1, 1), 2);
+    }
+}
